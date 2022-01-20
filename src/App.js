e69e9e8332b4c3a6eb445e30a66735de30d92be4
sumin <kim.sumin.wat2014@gmail.com>
2022-01-19 17:25:31 -0800

    added user component and connected to app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import React from 'react';
-
+import React from "react";
+import AddUser from "./components/Users/addUser";
 
 function App() {
   return (
     <div>
-
+      <AddUser />
     </div>
   );
 }
